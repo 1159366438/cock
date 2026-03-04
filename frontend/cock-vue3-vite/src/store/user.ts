25df22d92c24a04683829105f872f064e7c4da4a
ADMIN <1159366438@qq.com>
2026-03-04 20:44:16 +0800

    20260304 前端重构目录

--- frontend/cock-vue3-vite/src/store/user.ts
+++ frontend/cock-vue3-vite/src/store/user.ts
@@ -2,7 +2,7 @@
  * 用户状态管理
  */
 import { defineStore } from 'pinia'
-import { userApi } from '../api/punchApi'
+import { userApi } from '../api/userApi'
 import type { UserInfo } from '../types'
 
 export const useUserStore = defineStore('user', {
@@ -25,14 +25,15 @@ export const useUserStore = defineStore('user', {
       this.error = ''
       try {
         const res = await userApi.getUserInfo()
-        if (res.code === 200) {
-          this.userInfo = res.data
+        console.log('获取用户信息接口响应:', res)
+        if (res.status === 200) {
+          this.userInfo.name = res.data.username
         } else {
-          this.error = '获取用户信息失败'
+          this.error = '获取用户信息失败111111'
         }
       } catch (error) {
-        this.error = '获取用户信息时发生错误'
-        console.error('获取用户信息失败:', error)
+        this.error = '获取用户信息时发生错误111'
+        console.error('获取用户信息失败111:', error)
       } finally {
         this.loading = false
       }
